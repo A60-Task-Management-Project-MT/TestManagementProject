95be036e4d1a06af8d99172b3d7134c59a503f49
MarioBoykov <mario.georgiev.a60@learn.telerikacademy.com>
2024-05-12 19:34:38 +0300

    mario test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(324324);
+        System.out.println("Mario");
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,8 @@
 public class Task {
     public static void main(String[] args) {
         System.out.println(42342);
+        System.out.println(
+                6456
+        );
     }
 }
