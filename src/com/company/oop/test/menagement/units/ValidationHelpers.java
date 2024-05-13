d094b40d44796a1ba2b5fa37d3b9aa4c0bef6a11
MarioBoykov <mario.georgiev.a60@learn.telerikacademy.com>
2024-05-13 10:32:02 +0300

    Added ValidationHelpers and ParsingHelpers methods.

--- src/com/company/oop/test/menagement/units/ValidationHelpers.java
+++ src/com/company/oop/test/menagement/units/ValidationHelpers.java
@@ -1,4 +1,31 @@
 package com.company.oop.test.menagement.units;
 
+import java.util.List;
+
 public class ValidationHelpers {
+    private static final String INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of arguments. Expected: %d; received: %d.";
+
+    public static void validateIntRange(int value, int min, int max, String message) {
+        if (value < min || value > max) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void validateDecimalRange(double value, double min, double max, String message) {
+        if (value < min || value > max) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void validateArgumentsCount(List<String> list, int expectedNumberOfParameters) {
+        if (list.size() < expectedNumberOfParameters) {
+            throw new IllegalArgumentException(
+                    String.format(INVALID_NUMBER_OF_ARGUMENTS, expectedNumberOfParameters, list.size())
+            );
+        }
+    }
+
+    public static void validateStringLength(String stringToValidate, int minLength, int maxLength, String errorMessage) {
+        validateIntRange(stringToValidate.length(),minLength,maxLength,errorMessage);
+    }
 }
