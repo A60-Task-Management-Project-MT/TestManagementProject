e3621899fe9e17cb7ffce96b93f84680623fcd44
Todor Raynov <t.raynov@abv.bg>
2024-05-14 15:29:15 +0300

    Comment class added.

---
+++ src/com/company/oop/test/menagement/models/CommentImpl.java
@@ -0,0 +1,34 @@
+package com.company.oop.test.menagement.models;
+
+import com.company.oop.test.menagement.models.contracts.Comment;
+
+public class CommentImpl implements Comment {
+
+    private String author;
+    private String content;
+
+    public CommentImpl(String author, String content) {
+        this.author = author;
+        this.content = content;
+    }
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                **********
+                %s
+                User: %s
+                **********
+                """.formatted(content, author).trim();
+    }
+}
