87d6963409fdc97db91190b3f81f13311eba394e
Todor Raynov <t.raynov@abv.bg>
2024-05-14 17:21:21 +0300

    TaskManagementRepositoryImpl modified!

--- src/com/company/oop/test/menagement/core/TaskManagementRepositoryImpl.java
+++ src/com/company/oop/test/menagement/core/TaskManagementRepositoryImpl.java
@@ -1,20 +1,143 @@
 package com.company.oop.test.menagement.core;
 
 import com.company.oop.test.menagement.core.contracts.TaskManagementRepository;
-import com.company.oop.test.menagement.models.contracts.Board;
-import com.company.oop.test.menagement.models.contracts.Member;
-import com.company.oop.test.menagement.models.contracts.Task;
+import com.company.oop.test.menagement.models.BugImpl;
+import com.company.oop.test.menagement.models.FeedbackImpl;
+import com.company.oop.test.menagement.models.StoryImpl;
+import com.company.oop.test.menagement.models.contracts.*;
+import com.company.oop.test.menagement.models.enums.PriorityType;
+import com.company.oop.test.menagement.models.enums.bug_enums.BugSeverityType;
+import com.company.oop.test.menagement.models.enums.story_enums.StorySizeType;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskManagementRepositoryImpl implements TaskManagementRepository {
-    private int nextId;
-    private final List<Board> boards;
-    private final List<Task> tasks;
 
-    public TaskManagementRepositoryImpl(List<Board> boards, List<Task> tasks) {
-        this.boards = boards;
-        this.tasks = tasks;
+    public static final String TEAM_EXIST_ERROR = "Team %s does not exist!";
+    private int nextId;
+    private final List<Teams> teams = new ArrayList<>();
+    private final List<Task> tasks = new ArrayList<>();
+    private final List<Board> boards = new ArrayList<>();
+    private final List<Member> members = new ArrayList<>();
+
+    public TaskManagementRepositoryImpl() {
         this.nextId = 0;
+    }
+
+    @Override
+    public List<Task> getTask() {
+        return new ArrayList<>(tasks);
+    }
+
+    @Override
+    public List<Teams> getTeams() {
+        return new ArrayList<>(teams);
+    }
+
+    @Override
+    public List<Member> getMembers() {
+        return new ArrayList<>(members);
+    }
+
+    @Override
+    public List<Board> getBoards() {
+        return new ArrayList<>(boards);
+    }
+
+    @Override
+    public Bug createBug(String title, String description, PriorityType priorityType, BugSeverityType severityType, String assignee) {
+        Bug bug = new BugImpl(++nextId, title, description, priorityType, severityType, assignee);
+        tasks.add(bug);
+        return bug;
+    }
+
+    @Override
+    public Story createStory(String title, String description, PriorityType priorityType, StorySizeType storySizeType, String assignee) {
+        Story story = new StoryImpl(++nextId, title, description, priorityType, storySizeType, assignee);
+        tasks.add(story);
+        return story;
+    }
+
+    @Override
+    public Feedback createFeedback(String title, String description, int rating) {
+        Feedback feedback = new FeedbackImpl(++nextId, title, description, rating);
+        tasks.add(feedback);
+        return feedback;
+    }
+
+    @Override
+    public Teams createTeam(String name) {
+        return null;
+    }
+
+    @Override
+    public Member createMember(String memberName) {
+        return null;
+    }
+
+    @Override
+    public Board createBoard(String boardName) {
+        return null;
+    }
+
+    @Override
+    public Comment createComment(String author, String content) {
+        return null;
+    }
+
+    @Override
+    public Task findTaskById(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean teamExist(String teamName) {
+        return false;
+    }
+
+    @Override
+    public boolean boardExist(String boardName) {
+        return false;
+    }
+
+    @Override
+    public boolean memberExist(String memberName) {
+        return false;
+    }
+
+    @Override
+    public void addBoardToTeam(Board board) {
+
+    }
+
+    @Override
+    public void addMemberToTeam(Member member) {
+
+    }
+
+    @Override
+    public Teams findTeamByTeamName(String teamName) {
+        for (Teams team : teams) {
+            if (team.getName().equalsIgnoreCase(teamName)) {
+                return team;
+            }
+        }
+        throw new IllegalArgumentException(String.format(TEAM_EXIST_ERROR, teamName));
+    }
+
+    @Override
+    public Member findMemberByMemberName(String memberName) {
+        for (Member member : members) {
+            if(member.getMemberName().equalsIgnoreCase(memberName)) {
+                return member;
+            }
+        }
+        throw new IllegalArgumentException(String.format("Member %s already exist!", memberName));
+    }
+
+    @Override
+    public Board findBoardByBoardName(String boardName) {
+        return null;
     }
 }
--- src/com/company/oop/test/menagement/core/contracts/TaskManagementRepository.java
+++ src/com/company/oop/test/menagement/core/contracts/TaskManagementRepository.java
@@ -9,16 +9,42 @@ import java.util.List;
 
 public interface TaskManagementRepository {
     List<Task> getTask();
+
     List<Teams> getTeams();
-    Bug createBug(String title, String description, PriorityType priorityType, BugSeverityType severityType,String assignee);
+
+    List<Member> getMembers();
+
+    List<Board> getBoards();
+
+    Bug createBug(String title, String description, PriorityType priorityType, BugSeverityType severityType, String assignee);
+
     Story createStory(String title, String description, PriorityType priorityType, StorySizeType storySizeType, String assignee);
+
     Feedback createFeedback(String title, String description, int rating);
+
+    Teams createTeam(String name);
+
+    Member createMember(String memberName);
+
+    Board createBoard(String boardName);
+
+    Comment createComment(String author, String content);
+
     Task findTaskById(int id);
+
     boolean teamExist(String teamName);
+
     boolean boardExist(String boardName);
+
     boolean memberExist(String memberName);
+
     void addBoardToTeam(Board board);
+
     void addMemberToTeam(Member member);
+
     Teams findTeamByTeamName(String teamName);
+
     Member findMemberByMemberName(String memberName);
+
+    Board findBoardByBoardName(String boardName);
 }
