ab622b68e93a9909984183a98671a07514c558b1
MarioBoykov <mario.georgiev.a60@learn.telerikacademy.com>
2024-05-14 16:38:55 +0300

    Add methods to Repository

--- src/com/company/oop/test/menagement/core/TaskManagementRepositoryImpl.java
+++ src/com/company/oop/test/menagement/core/TaskManagementRepositoryImpl.java
@@ -1,6 +1,20 @@
 package com.company.oop.test.menagement.core;
 
 import com.company.oop.test.menagement.core.contracts.TaskManagementRepository;
+import com.company.oop.test.menagement.models.contracts.Board;
+import com.company.oop.test.menagement.models.contracts.Member;
+import com.company.oop.test.menagement.models.contracts.Task;
+
+import java.util.List;
 
 public class TaskManagementRepositoryImpl implements TaskManagementRepository {
+    private int nextId;
+    private final List<Board> boards;
+    private final List<Task> tasks;
+
+    public TaskManagementRepositoryImpl(List<Board> boards, List<Task> tasks) {
+        this.boards = boards;
+        this.tasks = tasks;
+        this.nextId = 0;
+    }
 }
--- src/com/company/oop/test/menagement/core/contracts/TaskManagementRepository.java
+++ src/com/company/oop/test/menagement/core/contracts/TaskManagementRepository.java
@@ -1,4 +1,24 @@
 package com.company.oop.test.menagement.core.contracts;
 
+import com.company.oop.test.menagement.models.contracts.*;
+import com.company.oop.test.menagement.models.enums.PriorityType;
+import com.company.oop.test.menagement.models.enums.bug_enums.BugSeverityType;
+import com.company.oop.test.menagement.models.enums.story_enums.StorySizeType;
+
+import java.util.List;
+
 public interface TaskManagementRepository {
+    List<Task> getTask();
+    List<Teams> getTeams();
+    Bug createBug(String title, String description, PriorityType priorityType, BugSeverityType severityType,String assignee);
+    Story createStory(String title, String description, PriorityType priorityType, StorySizeType storySizeType, String assignee);
+    Feedback createFeedback(String title, String description, int rating);
+    Task findTaskById(int id);
+    boolean teamExist(String teamName);
+    boolean boardExist(String boardName);
+    boolean memberExist(String memberName);
+    void addBoardToTeam(Board board);
+    void addMemberToTeam(Member member);
+    Teams findTeamByTeamName(String teamName);
+    Member findMemberByMemberName(String memberName);
 }
