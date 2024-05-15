e91c2ed454dfaf2f2d99bc743fa0fee6fdf74b66
Todor Raynov <t.raynov@abv.bg>
2024-05-15 17:01:24 +0300

    CreateNewBugToBoardCommand updated.

--- src/com/company/oop/test/menagement/commands/creation/CreateNewBugToBoardCommand.java
+++ src/com/company/oop/test/menagement/commands/creation/CreateNewBugToBoardCommand.java
@@ -3,6 +3,7 @@ package com.company.oop.test.menagement.commands.creation;
 import com.company.oop.test.menagement.commands.contracts.Command;
 import com.company.oop.test.menagement.core.contracts.TaskManagementRepository;
 import com.company.oop.test.menagement.models.BoardImpl;
+import com.company.oop.test.menagement.models.BugImpl;
 import com.company.oop.test.menagement.models.contracts.Board;
 import com.company.oop.test.menagement.models.contracts.Task;
 import com.company.oop.test.menagement.models.enums.PriorityType;
@@ -12,12 +13,13 @@ import com.company.oop.test.menagement.models.enums.bug_enums.BugStatusType;
 import com.company.oop.test.menagement.units.ParsingHelpers;
 import com.company.oop.test.menagement.units.ValidationHelpers;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CreateNewBugToBoardCommand implements Command {
 
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 8;
-    private static final String TASK_SUCCESSFULLY_ADDED_TO_BOARD = "Task %s with ID %s added to board %s!";
+    private static final String TASK_SUCCESSFULLY_ADDED_TO_BOARD = "Task %s with ID %d added to board %s!";
 
     private final TaskManagementRepository taskManagementRepository;
 
@@ -29,18 +31,17 @@ public class CreateNewBugToBoardCommand implements Command {
     public String execute(List<String> parameters) {
         ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
 
-        String boardName = parameters.get(0);
-
-        TaskType taskType = ParsingHelpers.tryParseEnum(parameters.get(1), TaskType.class);
-        String title = parameters.get(2);
-        String description = parameters.get(3);
-        PriorityType priorityType = ParsingHelpers.tryParseEnum(parameters.get(4), PriorityType.class);
-        BugSeverityType severityType = ParsingHelpers.tryParseEnum(parameters.get(5), BugSeverityType.class);
-        BugStatusType statusType = ParsingHelpers.tryParseEnum(parameters.get(6), BugStatusType.class);
-        String assignee = parameters.get(7);
+        TaskType taskType = ParsingHelpers.tryParseEnum(parameters.get(0), TaskType.class);
+        String title = parameters.get(1);
+        String description = parameters.get(2);
+        PriorityType priorityType = ParsingHelpers.tryParseEnum(parameters.get(3), PriorityType.class);
+        BugSeverityType severityType = ParsingHelpers.tryParseEnum(parameters.get(4), BugSeverityType.class);
+        String assignee = parameters.get(5);
+        String boardName = parameters.get(6);
+        List<String> steps = Arrays.asList(parameters.get(7).split("; "));
 
         Board board = taskManagementRepository.findBoardByBoardName(boardName);
-        Task task = createTask(taskType, title, description, priorityType, severityType, statusType, assignee);
+        Task task = createTask(taskType, title, description, priorityType, severityType, assignee, steps);
 
         board.addTask(task);
 
@@ -48,7 +49,7 @@ public class CreateNewBugToBoardCommand implements Command {
     }
 
     private Task createTask(TaskType taskType, String title, String description, PriorityType priorityType,
-                           BugSeverityType severityType, BugStatusType statusType, String assignee) {
-        return taskManagementRepository.createBug(title, description, priorityType, severityType, statusType, assignee);
+                           BugSeverityType severityType, String assignee, List<String> steps) {
+        return taskManagementRepository.createBug(title, description, priorityType, severityType, assignee, steps);
     }
 }
