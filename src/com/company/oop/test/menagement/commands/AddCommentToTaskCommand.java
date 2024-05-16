99cb71a60b6af368ac7a8bbaa715cf6661e13f32
MarioBoykov <mario.georgiev.a60@learn.telerikacademy.com>
2024-05-16 10:17:53 +0300

    Add CreateNewPersonCommand

--- src/com/company/oop/test/menagement/commands/AddCommentToTaskCommand.java
+++ src/com/company/oop/test/menagement/commands/AddCommentToTaskCommand.java
@@ -10,6 +10,7 @@ import com.company.oop.test.menagement.units.ValidationHelpers;
 import java.util.List;
 
 public class AddCommentToTaskCommand implements Command {
+
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 3;
     private static final String INVALID_PARAMETER_ERROR = "You need task ID as number to write comment!";
     private TaskManagementRepository taskManagementRepository;
@@ -17,22 +18,24 @@ public class AddCommentToTaskCommand implements Command {
     public AddCommentToTaskCommand(TaskManagementRepository taskManagementRepository) {
         this.taskManagementRepository = taskManagementRepository;
     }
+
     @Override
     public String execute(List<String> parameters) {
-        ValidationHelpers.validateArgumentsCount(parameters,EXPECTED_NUMBER_OF_ARGUMENTS);
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
 
         String author = parameters.get(0);
         String content = parameters.get(1);
         int taskId = ParsingHelpers.tryParseInt(parameters.get(2), INVALID_PARAMETER_ERROR);
 
-        Comment comment = createComment(author,content);
+        Comment comment = createComment(author, content);
         Task task = taskManagementRepository.findTaskById(taskId);
 
         task.addComment(comment);
 
-        return String.format("Comment successfully added to task %s with ID %d!",task.getTaskType(),taskId);
+        return String.format("Comment successfully added to task %s with ID %d!", task.getTaskType(), taskId);
     }
+
     private Comment createComment(String author, String content) {
-        return taskManagementRepository.createComment(author,content);
+        return taskManagementRepository.createComment(author, content);
     }
 }
--- src/com/company/oop/test/menagement/commands/CreateNewPersonCommand.java
+++ src/com/company/oop/test/menagement/commands/CreateNewPersonCommand.java
@@ -1,17 +1,37 @@
 package com.company.oop.test.menagement.commands;
 
 import com.company.oop.test.menagement.commands.contracts.Command;
+import com.company.oop.test.menagement.core.contracts.TaskManagementRepository;
+import com.company.oop.test.menagement.exceptions.DuplicateEntityException;
+import com.company.oop.test.menagement.units.ValidationHelpers;
 
 import java.util.List;
+import java.util.zip.DataFormatException;
 
 
 public class CreateNewPersonCommand implements Command {
 
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
+    private TaskManagementRepository taskManagementRepository;
 
-
+    public CreateNewPersonCommand(TaskManagementRepository taskManagementRepository) {
+        this.taskManagementRepository = taskManagementRepository;
+    }
 
     @Override
     public String execute(List<String> parameters) {
-        return "";
+        ValidationHelpers.validateArgumentsCount(parameters,EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        String personName = parameters.get(0);
+
+        return createPerson(personName);
+    }
+
+    private String createPerson(String personName) {
+        if (taskManagementRepository.memberExist(personName)) {
+            throw new DuplicateEntityException(String.format("Person with name %s already exist!", personName));
+        }
+        taskManagementRepository.createMember(personName);
+        return String.format("Person with name %s was created!", personName);
     }
 }
