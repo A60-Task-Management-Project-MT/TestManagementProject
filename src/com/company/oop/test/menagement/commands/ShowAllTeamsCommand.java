18bf64365fb8cca958ceeca1cfa6cfba782ed21e
MarioBoykov <mario.georgiev.a60@learn.telerikacademy.com>
2024-05-16 13:56:53 +0300

    Add ShowAllTeamsCommand

--- src/com/company/oop/test/menagement/commands/ShowAllTeamsCommand.java
+++ src/com/company/oop/test/menagement/commands/ShowAllTeamsCommand.java
@@ -1,4 +1,30 @@
 package com.company.oop.test.menagement.commands;
 
-public class ShowAllTeamsCommand {
+import com.company.oop.test.menagement.commands.contracts.Command;
+import com.company.oop.test.menagement.core.contracts.TaskManagementRepository;
+import com.company.oop.test.menagement.models.contracts.Board;
+import com.company.oop.test.menagement.models.contracts.Teams;
+
+import java.util.List;
+
+public class ShowAllTeamsCommand implements Command {
+
+    private TaskManagementRepository taskManagementRepository;
+
+    public ShowAllTeamsCommand(TaskManagementRepository taskManagementRepository) {
+        this.taskManagementRepository = taskManagementRepository;
+    }
+
+    @Override
+    public String execute(List<String> parameters) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        sb.append("~~~ TEAMS ~~~").append(System.lineSeparator());
+        for (Teams team : taskManagementRepository.getTeams()) {
+            sb.append(count).append(". ");
+            sb.append(team).append(System.lineSeparator());
+            count++;
+        }
+        return sb.toString().trim();
+    }
 }
