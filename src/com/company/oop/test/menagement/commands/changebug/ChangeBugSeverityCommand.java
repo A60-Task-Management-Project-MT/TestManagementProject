454b38c13a77120444ba31f618894ea16e149bf6
MarioBoykov <mario.georgiev.a60@learn.telerikacademy.com>
2024-05-16 09:04:39 +0300

    Add ChangeBugSeverityCommand.

--- src/com/company/oop/test/menagement/commands/changebug/ChangeBugSeverityCommand.java
+++ src/com/company/oop/test/menagement/commands/changebug/ChangeBugSeverityCommand.java
@@ -1,4 +1,35 @@
 package com.company.oop.test.menagement.commands.changebug;
 
-public class ChangeBugSeverityCommand {
+import com.company.oop.test.menagement.commands.contracts.Command;
+import com.company.oop.test.menagement.core.contracts.TaskManagementRepository;
+import com.company.oop.test.menagement.models.contracts.Task;
+import com.company.oop.test.menagement.models.enums.bug_enums.BugSeverityType;
+import com.company.oop.test.menagement.units.ParsingHelpers;
+import com.company.oop.test.menagement.units.ValidationHelpers;
+
+import java.util.List;
+
+public class ChangeBugSeverityCommand implements Command {
+
+    public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    public static final String ID_ERROR_MESSAGE = "ID needs to be a number!";
+    public static final String BUG_SEVERITY_CHANGE_MESSAGE = "Severity of task %s with ID %d was changed to %s!";
+
+    private final TaskManagementRepository taskManagementRepository;
+
+    public ChangeBugSeverityCommand(TaskManagementRepository taskManagementRepository) {
+        this.taskManagementRepository = taskManagementRepository;
+    }
+    @Override
+    public String execute(List<String> parameters) {
+        ValidationHelpers.validateArgumentsCount(parameters,EXPECTED_NUMBER_OF_ARGUMENTS);
+
+        int id = ParsingHelpers.tryParseInt(parameters.get(0),ID_ERROR_MESSAGE);
+        BugSeverityType newSeverityType = ParsingHelpers.tryParseEnum(parameters.get(1),BugSeverityType.class);
+
+        Task task = taskManagementRepository.findTaskById(id);
+        task.changeSeverity(newSeverityType);
+
+        return String.format(BUG_SEVERITY_CHANGE_MESSAGE, task.getTaskType(), task.getId(), newSeverityType);
+    }
 }
